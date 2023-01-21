5cd4afde9546194d0bcb734666e5a3cb728e780c
Nemuel Wainaina <bR0S3@protonmail.com>
2023-01-21 12:11:39 +0300

    Write the code

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,53 @@
 <script>
-	
+	let count = 1
+
+	const increment = () => {
+		count++
+	}
+
+	const decrement = () => {
+		count--
+	}
 </script>
 
 <main>
-	Hello World
+    <p>
+        {count}
+    </p>
+    <button id="inc" on:click={increment}>+</button>
+    <button id="dec" on:click={decrement}>-</button>
 </main>
 
 <style>
 	main {
+		color: white;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		max-width: 500px;
+		max-height: 300px;
 		margin: 0 auto;
+		font-size: 200%;
+		background-color: blue;
+		padding: 35px
+	}
+
+	button {
+		height: 70px;
+		width: 70px;
+		color: white;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+
+	#inc {
+		background-color: green;
+	}
+
+	#dec {
+		background-color: red;
 	}
 </style>
